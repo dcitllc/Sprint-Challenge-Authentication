43dc37515201d944166b2ad637f154e0bc52c334
Daniel Chamorro <dan@dcitllc.com>
2018-08-17 16:36:51 -0400

    require db

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,12 @@
-const axios = require('axios');
+const axios = require("axios");
+const db = require("../database/dbConfig");
 
-const { authenticate } = require('./middlewares');
+const { authenticate } = require("./middlewares");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
@@ -19,12 +20,12 @@ function login(req, res) {
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
